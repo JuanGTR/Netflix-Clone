665bbb97a3f84529aeda39b49e493d60e7d33843
Juan Tejeda <juanga.tejeda@gmail.com>
2025-04-19 20:59:30 -0400

    Ready to start firebase login 2:17

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,12 +1,22 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
-
-
+import { Link } from 'react-router-dom';
 
 const TitleCards = ({title, category}) => {
 
+    const [apiData, setApiData] = useState([]);
     const cardsRef = useRef();
+
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYzhkZTc1NTc0MzNkZGE4ZmE5OTJhMWE3YzRiMjJhOCIsIm5iZiI6MTc0NTEwNTMzNi43NTUsInN1YiI6IjY4MDQzMWI4NTU3MGNiYTllMWFkM2QzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kXHQb10H8yGLfZZ0Nh3WVAYbzdzH8KmjqOwxQr0_gy0'
+        }
+      };
+      
+      
 
 const handleWheel = (event)=>{
     event.preventDefault();
@@ -15,17 +25,22 @@ const handleWheel = (event)=>{
 
 useEffect(()=>{
 
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+        .then(res => res.json())
+        .then(res => setApiData(res.results))
+        .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel', handleWheel)
 },[])
   return (
     <div className='title-cards'>
         <h2>{title?title:"Popular on Netlfix"}</h2>
         <div className="card-list" ref={cardsRef}>
-            {cards_data.map((card, index)=>{
-                return <div className="card" key={index}>
-                    <img src={card.image} alt="" />
-                    <p>{card.name}</p>
-                </div>
+            {apiData.map((card, index)=>{
+                return <Link to={`/player/${card.id}`} className="card" key={index}>
+                    <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+                    <p>{card.original_title}</p>
+                </Link>
             })}
         </div>
     </div>
